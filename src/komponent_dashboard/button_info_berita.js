2c89b456a3ce39b3205f8d273d52c708090878b2
ikhwaDEV1528 <ikhwanmarditya123@student.esaunggul.ac.id>
2026-01-04 06:48:09 +0700

    mem

--- src/komponent_dashboard/button_info_berita.js
+++ src/komponent_dashboard/button_info_berita.js
@@ -1,29 +1,23 @@
 'use client'
 
-import { LottiePlayer } from "@lottiefiles/lottie-player";
 import { useRouter } from "next/navigation";
-import { useState,useEffect } from "react";
-import Lottie from "react-lottie-player";
+import dynamic from 'next/dynamic';
 import info from '@/gambar-project/info.json'
 
-export function Button_info_berita(){
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false })
 
+export function Button_info_berita() {
     const navigasi = useRouter()
 
     return(
-        // <button onClick={()=> navigasi.push('/page_info&berita')} className="flex flex-col bg-slate-900 text-white rounded-md w-52 font-bold border-white border-2 ml-2 mt-4 ">
-        //  Informasi & Berita Penting 
-        // <p className="text-[11px] italic text-yellow-400">klik untuk lihat berita kasus terkini</p>
-        // </button>
-        <main onClick={()=> navigasi.push('/page_info&berita')} className="flex flex-col">
-         <Lottie
-           play 
-           animationData={info}
-           loop={true}
-           className="w-15 absolute ml-75 mt-6"
-        />
-          <p className="absolute ml-75 mt-20 text-[12px] text-slate-900 italic">Berita & Info</p>
+        <main onClick={() => navigasi.push('/page_info&berita')} className="flex flex-col relative cursor-pointer">
+            <Lottie
+                play 
+                animationData={info}
+                loop={true}
+                className="w-15 absolute ml-75 mt-6"
+            />
+            <p className="absolute ml-75 mt-20 text-[12px] text-slate-900 italic">Berita & Info</p>
         </main>
-        
     )
 }
